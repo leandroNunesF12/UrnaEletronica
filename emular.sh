d7906ea38acc8864fe8cb2eff77c2208de5c8547
leandro nunes <leandronunes305@gmail.com>
2022-06-05 20:10:18 -0300

    ínicio do projeto da urna

---
+++ emular.sh
@@ -0,0 +1,5 @@
+ionic cordova platform add android --force
+ionic cordova resources android --force
+ionic cordova prepare android --force
+ionic cordova compile android
+ionic cordova emulate android
